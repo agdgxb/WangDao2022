50948ed4b3afb97428e8940d3365e3bbf451f07f
Yao-bos <92080413+agdgxb@users.noreply.github.com>
2022-07-14 17:26:40 +0800

    Update List.h

--- DataStructure/List.h
+++ DataStructure/List.h
@@ -1,1 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+#定义单链表
+typedef struct LNode {
+  int data;
+  struct LNode* next;
+}LNode, *LinkList;
+
+#初始化不带头结点的单链表
+bool initList_nohead(LinkList &L){
+  #不必初始化，直接NULL
+  L = NULL;
+  return true;
+}
+
+#初始化带头结点的单链表
+bool initList_withhead(LinkList &L){
+  L = (LNode *) malloc (sizeof(LNode));
+  if ( L == NULL )  #未能成功分配空间
+     return false;
+  L -> next = NULL;
+  return true;
+}
+
+#判断单链表是否为空
+bool empty(LinkList L){
+  #不带头结点
+  return (L == NULL);
+  #带头结点
+  if(L -> next == NULL)
+    return true;
+  else
+    return false;
+}
