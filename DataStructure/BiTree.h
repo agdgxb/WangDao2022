2cc6beb05cacfaa19086bf04c3bd490845a26a12
Yao-bos <92080413+agdgxb@users.noreply.github.com>
2022-07-24 15:38:27 +0800

    Update BiTree.h

--- DataStructure/BiTree.h
+++ DataStructure/BiTree.h
@@ -49,3 +49,80 @@ void LevelOrder(BiTree T){
       EnQueue(Q,p->rchild);
   }
 }
+
+
+#线索二叉树结点
+typedef struct ThreadNode{
+  ElemType data;
+  struct ThreadNode *lchild,*rchild;
+  int ltag,rtag;
+}ThreadNode,*ThreadTree;
+
+#中序遍历对二叉树线索化
+ThreadNode *pre = NULL;   #定义一个全局变量
+void InThread(ThreadTree T){
+  if(T != NULL){
+    InThread(T->lchild);  #中序遍历左子树
+    visit(T);  #访问根节点
+    InThread(T->rchild);
+  }
+}
+
+void visit(BiTree T){
+  if(T->lchild == NULL){
+    T>lchild = pre;
+    T->ltag = 1;
+  }
+  if(pre != NULL && T->rchild == NULL){
+    pre->rchild = T;
+    pre->rtag = 1;
+  }
+  pre = p;
+}
+
+#中序线索化二叉树
+void CreateInThread(BiTree){
+  pre = NULL;
+  if(T != NULL){  #非空二叉树才能线索化
+    InThread(T);   #中序线索化
+    if(pre->rchild == NULL)  #处理最后一个结点
+      pre->rtag = 1;
+  }
+}
+
+#先序线索化二叉树,仍然利用visit函数,Create函数也不变
+#注意可能会出现lchild指向前一个结点的问题
+void PreThread(BiTree T){
+  if(T != NULL){
+    visit(T);
+    if(T->ltag == 0)  #判断lchild不是前驱线索才遍历左子树
+      PreThread(T->lchild);
+    PreThread(T->rchild);
+  }
+}
+
+#找以P为根的子树中,第一个被中序遍历的结点,即P的最左下结点
+ThreadNode *FirstNode(ThreadNode *p){
+  if(p->ltag == 0)
+    p = p->lchild;
+  return p;
+}
+#在中序线索二叉树中找到结点p的后继结点
+ThreadNode *NextNode(ThreadNode *p){
+  if(p->rtag == 0)  #如果p的右结点不是指向后继
+    return FirstNode(p->rchild);
+  else
+    return p->rchild;
+}
+
+
+
+
+
+
+
+
+
+
+
+
